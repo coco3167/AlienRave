9b7bc257c1955a1adb31d24f15498b26d794ede5
Wislia <104387292+Wislia@users.noreply.github.com>
2024-12-11 20:40:24 +0100

    meshtrail script updated

--- Assets/Materials/Shader/MeshTrail.cs
+++ Assets/Materials/Shader/MeshTrail.cs
@@ -5,23 +5,30 @@ public class MeshTrail : MonoBehaviour
 {
     public float activeTime = 2;
 
-    [Header("Mesh related")]
+    [Header("Mesh")]
     public float meshRefreshRate = 0.1f;
+    public float meshDestroyDelay = 3f;
+    public Transform positionToSpawn;
 
+    [Header("Shader")]
+    public Material mat;
+    public string shaderVarRef;
+    public float shaderVarRate = 0.1f;
+    public float shaderVarRefresh = 0.05f;
+
+    private bool isTrailActive;
     private SkinnedMeshRenderer[] skinnedMeshRenderers;
 
-
-    // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Space) && !isTrailActive)
         {
-            // NANI ?!
+            isTrailActive = true;
             StartCoroutine(ActivateTrail(activeTime));
         }
     }
 
-    IEnumerator ActivateTrail (float timeActive)
+    IEnumerator ActivateTrail(float timeActive)
     {
         while (timeActive > 0)
         {
@@ -32,14 +39,41 @@ public class MeshTrail : MonoBehaviour
                 skinnedMeshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
             }
 
-            for (int i = 0; i<skinnedMeshRenderers.Length; i++)
+            for (int i = 0; i < skinnedMeshRenderers.Length; i++)
             {
                 GameObject gObj = new GameObject();
+                gObj.transform.SetPositionAndRotation(positionToSpawn.position, positionToSpawn.rotation);
 
-                
+                MeshRenderer mr = gObj.AddComponent<MeshRenderer>();
+                MeshFilter mf = gObj.AddComponent<MeshFilter>();
+
+                Mesh mesh = new Mesh();
+                skinnedMeshRenderers[i].BakeMesh(mesh);
+
+                mf.mesh = mesh;
+                mr.material = mat;
+
+                StartCoroutine(AnimateMaterialfloat(mr.material, 0, shaderVarRate, shaderVarRefresh));
+
+                Destroy(gObj, meshDestroyDelay);
             }
 
             yield return new WaitForSeconds(meshRefreshRate);
+        }
+
+        isTrailActive = false;
+    }
+
+    IEnumerator AnimateMaterialfloat(Material mat, float goal, float rate, float refreshRate)
+    {
+        float valueToAnimate = mat.GetFloat(shaderVarRef);
+
+        while (valueToAnimate > goal)
+        {
+            valueToAnimate -= rate;
+            mat.SetFloat(shaderVarRef, valueToAnimate);
+
+            yield return new WaitForSeconds(refreshRate);
         }
     }
 }
